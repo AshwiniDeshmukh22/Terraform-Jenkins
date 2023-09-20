9b46550b743417dd2f962a5fded5117f104fa2bc
AshwiniDeshmukh22 <143481863+AshwiniDeshmukh22@users.noreply.github.com>
2023-09-20 17:38:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,3 +49,7 @@ resource "aws_route_table" "PublicRT"{
         Name = "MyRT"
     }
 }
+resource "aws_route_table_association" "PublicRTAssociation"{
+    subnet_id = aws_subnet.PublicSubnet.id
+    route_table_id = aws_route_table.PublicRT.id
+}
