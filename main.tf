ed18449ba3e00aefb13e936d9849d0c02d6d4fa6
AshwiniDeshmukh22 <143481863+AshwiniDeshmukh22@users.noreply.github.com>
2023-09-20 17:55:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,4 +59,7 @@ resource "aws_eip" "nat_eip" {
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.PrivSubnet.id
+    tags = {
+        Name = "MyNAT"
+    }
 }
