beacc45d57c3abaa623cc56806173f0ba27a90f6
AshwiniDeshmukh22 <143481863+AshwiniDeshmukh22@users.noreply.github.com>
2023-09-20 17:51:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,3 +56,7 @@ resource "aws_route_table_association" "PublicRTAssociation"{
 resource "aws_eip" "nat_eip" {
   vpc = true
 }
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.PrivSubnet.id
+}
