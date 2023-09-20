5aa238f353a60d72be899b6e2b3c388e38dbd4fe
AshwiniDeshmukh22 <143481863+AshwiniDeshmukh22@users.noreply.github.com>
2023-09-20 17:25:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,3 +29,11 @@ resource "aws_subnet" "PrivSubnet"{
 resource "aws_internet_gateway" "myIgw"{
     vpc_id = aws_vpc.myvpc.id
 }
+# route Tables for public subnet
+resource "aws_route_table" "PublicRT"{
+    vpc_id = aws_vpc.myvpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myIgw.id
+    }
+}
