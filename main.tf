ea38b6885817dbacaa39d31332f41cab2def863b
AshwiniDeshmukh22 <143481863+AshwiniDeshmukh22@users.noreply.github.com>
2023-09-20 17:15:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,8 @@ resource "aws_vpc" "myvpc"{
         Name = "MyVPC"
     }
 }
+resource "aws_subnet" "PublicSubnet"{
+    vpc_id = aws_vpc.myvpc.id
+    availability_zone = "ap-south-1"
+    cidr_block = "10.0.1.0/24"
+}
