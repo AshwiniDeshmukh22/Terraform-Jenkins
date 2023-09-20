a842163fc7e146f6d35b6a6bb1b3b468a15731b7
AshwiniDeshmukh22 <143481863+AshwiniDeshmukh22@users.noreply.github.com>
2023-09-20 17:22:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,9 @@ resource "aws_subnet" "PublicSubnet"{
     availability_zone = "ap-south-1a"
     cidr_block = "10.0.1.0/24"
 }
+resource "aws_subnet" "PrivSubnet"{
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = true
+
+}
